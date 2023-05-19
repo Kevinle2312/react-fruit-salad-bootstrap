4d0fa737663515cb6dee34a243d6b8d211a96568
Le Cao Tung Lam <Kevinle231299@gmail.com>
2023-05-19 10:05:27 +0200

    Project - Panier fruit

--- src/App.js
+++ src/App.js
@@ -5,57 +5,60 @@ import { useState } from "react";
 import Panier from "./components/Panier";
 
 function App() {
-  const [displayFruits, setDisplayFruits] = useState(false);
-  const [selectedFruits, setSelectedFruits] = useState([]);
+    const [displayFruits, setDisplayFruits] = useState(false);
+    const [selectedFruits, setSelectedFruits] = useState([]);
 
-  const handleToggleSelection = (fruitId) => {
-      setSelectedFruits((prevSelectedFruits) => {
-          if (prevSelectedFruits.includes(fruitId)) {
-              return prevSelectedFruits.filter((id) => id !== fruitId);
-          } else {
-              return [...prevSelectedFruits, fruitId];
-          }
-      });
-  };
+    const handleToggleSelection = (fruitId) => {
+        setSelectedFruits((prevSelectedFruits) => {
+            if (prevSelectedFruits.includes(fruitId)) {
+                return prevSelectedFruits.filter((id) => id !== fruitId);
+            } else {
+                return [...prevSelectedFruits, fruitId];
+            }
+        });
+    };
 
-  function onClick() {
-    setDisplayFruits(!displayFruits);
-  }
+    function onClick() {
+        setDisplayFruits(!displayFruits);
+    }
 
-  return (
-      <div className="App">
-          <button onClick={onClick}>Afficher / Masquer</button>
+    return (
+        <div className="App">
+            <button onClick={onClick}>Afficher / Masquer</button>
 
-          {displayFruits ? (
-              <div>
-                  <Router>
-                      <div>
-                          <nav>
-                              <ul>
-                                  <li>
-                                      <Link to="/">Accueil</Link>
-                                  </li>
-                                  <li>
-                                      <Link to="/panier">Panier</Link>
-                                  </li>
-                              </ul>
-                          </nav>
-                          <Switch>
-                              <Route exact path="/">
-                                  <FruitsMaster />
-                              </Route>
-                              <Route path="/panier">
-                                  <Panier selectedFruits={selectedFruits}/>
-                              </Route>
-                          </Switch>
-                      </div>
-                  </Router>
-              </div>
-          ) : (
-              <p>Aucun fruit</p>
-          )}
-      </div>
-  );
+            {displayFruits ? (
+                <div>
+                    <Router>
+                        <div>
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Accueil</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/panier">Panier</Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <Switch>
+                                <Route exact path="/">
+                                    <FruitsMaster
+                                        selectedFruits={selectedFruits}
+                                        onToggleSelection={handleToggleSelection}
+                                    />
+                                </Route>
+                                <Route path="/panier">
+                                    <Panier selectedFruits={selectedFruits} />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Router>
+                </div>
+            ) : (
+                <p>Aucun fruit</p>
+            )}
+        </div>
+    );
 }
 
 export default App;
